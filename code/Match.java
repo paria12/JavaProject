ab9c7c9c83f141da72492218148f29a1f32533ff
léo <leo.babaux@etu.iut-tlse3.fr>
2022-12-01 11:58:45 +0100

    V Match/poule - 01/12/2022

--- code/Match.java
+++ code/Match.java
@@ -1,19 +1,19 @@
 package code;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Time;
 
 import javax.sql.DataSource;
 
 public class Match {
-	private Date Hdebut;
-	private Date Hfin;
+	private Time Hdebut;
+	private Time Hfin;
 	private Equipe E1;
 	private Equipe E2;
-	
-	public Match(Equipe e1, Equipe e2, Date hd, Date hf) {
+
+	public Match(Equipe e1, Equipe e2, Time hd, Time hf) {
 		if (e1 == null || e2 == null || hd == null || hf == null) {
 			throw new IllegalArgumentException("L'un des paramètres est null");
 		} else if (hd.compareTo(hf) <= 0) {
@@ -25,11 +25,11 @@ public class Match {
 		this.Hfin = hf;
 	}
 
-	public Date getHDebut() {
+	public Time getHDebut() {
 		return Hdebut;
 	}
 
-	public Date getHFin() {
+	public Time getHFin() {
 		return Hfin;
 	}
 
@@ -40,19 +40,37 @@ public class Match {
 	public Equipe getEquipe2() {
 		return E2;
 	}
-	
+
+	public void setWiner(Equipe e) throws ErreurBD {
+		if (!e.equals(this.E1)&&!e.equals(this.E2)) {
+			throw new IllegalArgumentException("L'equipe est invalide");
+		}
+		try {
+			DataSource bd = new ConnexionBD();
+
+			Connection connx = bd.getConnection();
+
+			Statement st = connx.createStatement();
+
+			st.executeQuery("Update match set gagnant = "+e.getID()+" where ID_equipe ="+this.E1.getID()+" and ID_equipe1 ="+this.E2.getID()+"and heuredebut ="+this.Hdebut+")");
+
+		} catch (SQLException exc) {
+			throw new ErreurBD("Erreur de requéte a la bd");
+		}
+	}
+
 	public void Insert(int poule) throws ErreurBD, IllegalArgumentException{
 		try {
-				DataSource bd = new ConnexionBD();
-				
-				Connection connx = bd.getConnection();
+			DataSource bd = new ConnexionBD();
 
-				Statement st = connx.createStatement();
+			Connection connx = bd.getConnection();
 
-				st.executeQuery("INSERT INTO match values("+this.E1.getID()+","+this.E2.getID()+","+poule+",null,"+this.Hdebut+","+this.Hfin+")");
+			Statement st = connx.createStatement();
 
-			} catch (SQLException e) {
-				throw new ErreurBD("Erreur de requéte a la bd");
-			}
+			st.executeQuery("INSERT INTO match values("+this.E1.getID()+","+this.E2.getID()+","+poule+",null,"+this.Hdebut+","+this.Hfin+")");
+
+		} catch (SQLException e) {
+			throw new ErreurBD("Erreur de requéte a la bd");
+		}
 	}
 }
--- code/Poule.java
+++ code/Poule.java
@@ -1,5 +1,63 @@
 package code;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+
+import javax.sql.DataSource;
+
 public class Poule {
+    
+    private Equipe[] equipes;
+    private Match[] matchs;
+    private int pointeur;
+    
+    public Poule (){
+        this.equipes = new Equipe[4];
+        this.matchs = new Match[6];
+        this.pointeur = 0;
+    }
+    
+    
+    public void InsérerEquipe(Equipe e) throws IllegalArgumentException{
+        for (Equipe i : this.equipes) {
+            if (i.equals(e)) {
+                throw new IllegalArgumentException("L'equipe est déjà présente dans la poule");
+            }
+            
+        }
+        if (this.pointeur == 4) {
+            throw new IllegalArgumentException("La poule est déjà pleine");
+        } else {
+            this.equipes[pointeur] = e;
+            this.pointeur++;
+        }
+    }
+    
+    public void insert(int tournoi) throws ErreurBD {
+            try {
+                DataSource bd = new ConnexionBD();
+                
+                Connection connx = bd.getConnection();
+
+                Statement st = connx.createStatement();
+                
+                st.executeQuery("Insert into Poule Values(seq_poule.nextVal, "+tournoi+")");
+                
+                for (Equipe e : this.equipes) {
+                    st.executeQuery("Insert into Participer Values ( "+e.getID()+ ", seq_poule.CURRVAL");
+                }
+            }catch (SQLException e){
+                throw new ErreurBD("Erreur de requete a la bd");
+            }    
+    }
+    
+    
+
+    public void GenererMatch(Time hdebut) {
+        this.matchs[0] = new Match(this.equipes[0],this.equipes[1],hdebut,hdebut);
+    }
+    
 
 }
